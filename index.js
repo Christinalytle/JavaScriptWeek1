12057efbb784366e599c4103c69622825d707846
Christina lytle <christina.lytle17@gmail.com>
2021-02-02 16:09:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var itemPrice = 50.00;
+var moneyInWallet = 100.00;
+var numberOfFriends = 6;
+var ageInYears = 30; 
+var firstName = "Kip"; 
+var lastName = "Lytle";
+var middleInitial = "D"; 
+
+
+var moneyInWalletAfterBuyingItem = moneyInWallet - itemPrice; 
+var friendsEachYear = numberOfFriends/ageInYears; 
+var fullName = firstName + " " + middleInitial + " " + lastName;
+
+console.log("I now have " + moneyInWalletAfterBuyingItem + " dollars after buying baby cloths"); 
+console.log("I have made " + friendsEachYear + " friends for each year of my life"); 
+console.log("My dog's full name is: " + fullName); 
